22cf56794792f9cca4629252a420cadfaf418e78
sub77 <sub77@ymail.com>
2015-05-07 02:07:05 +0200

    update
    Change-Id: I21d6d5a28062896bf2c1b2faa75925f81db16aa2

--- cm.mk
+++ cm.mk
@@ -10,3 +10,4 @@ $(call inherit-product, device/samsung/matisselte/full_matisselte.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_matisselte
+PRODUCT_BRAND := samsung
